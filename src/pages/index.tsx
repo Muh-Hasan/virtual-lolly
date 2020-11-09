ecc66b0f5620a981088b5a6e560595fe7dc8f05d
Muh-Hasan <muh.hasan.git@gmail.com>
2020-11-09 18:44:41 +0500

    basic setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Lolly from '../svg/lolly.svg'
+
+export default function Home() {
+  return (
+    <div>
+      <div>Hello world!</div>
+      <div>
+        <img src={Lolly} />
+      </div>
+    </div>
+  )
+}
