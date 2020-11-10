2b9ee69c0133c593d18a826a4ec9e9179156d2f1
Muh-Hasan <muh.hasan.git@gmail.com>
2020-11-10 21:11:59 +0500

    css done

--- src/components/dynamicPage/index.tsx
+++ src/components/dynamicPage/index.tsx
@@ -2,11 +2,11 @@ import React from "react"
 import Lolly from "../lolly"
 import gql from 'graphql-tag'
 import Header from "../header"
-import { useQuery } from "@apollo/client"
+import { Link } from 'gatsby'
 
 export const query = gql`
   query MyQuery($lollyPath: String!) {
-    LOLLIES {
+    lollies {
       getLollyByPath(lollyPath: $lollyPath) {
         flavorBot
         flavorMid
@@ -20,20 +20,37 @@ export const query = gql`
   }
 `
 
-export default function LollyPage() {
-    const { data } = useQuery(query)
-    console.log(data);
+export default function LollyPage({ data }) {
     
   return (
     <div>
-        <div>
-            <Header />
-        </div>
-        <div>
-            your link
-        </div>
-        <div>
-            {/* <Lolly/> */}
+      <Header />
+      <div className="freezedLollyCardWrapper">
+          <Lolly
+            fillLollyTop={data?.lollies.getLollyByPath?.flavourTop}
+            fillLollyBottom={data?.lollies.getLollyByPath?.flavourBot}
+            fillLollyMiddle={data?.lollies.getLollyByPath?.flavourMid}
+          />
+
+          <div className="freezedLollyData">
+            <div className="linkWrapper">
+              <h4>Share this link with your frined</h4>
+              <p>{`/lollies/${data?.lollies.getLollyByPath?.lollyPath}`}</p>
+            </div>
+            <div className="freezedLollyCard">
+              <h1>to: {data?.lollies.getLollyByPath?.recipientName}</h1>
+              <p>{data?.lollies.getLollyByPath?.message}</p>
+              <h3>From: {data?.lollies.getLollyByPath?.sendersName}</h3>
+            </div>
+            <div className="recivermessage">
+              <p>
+                {data?.lollies.getLollyByPath?.sendersName} made this virtual lollipop for you.
+                You can <Link to="/createLolly"> make your own</Link> to send to
+                a friend who deserve some sugary treat which won't rot their
+                teeth...
+              </p>
+            </div>
+          </div>
         </div>
     </div>
   )
