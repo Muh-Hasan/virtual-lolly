de35f8f3ef62cef08371684bd646bc10139ae63a
Muh-Hasan <muh.hasan.git@gmail.com>
2020-11-11 14:44:19 +0500

    upated

--- functions/newLolly/newLolly.js
+++ functions/newLolly/newLolly.js
@@ -1,7 +1,8 @@
 const { ApolloServer, gql } = require("apollo-server-lambda")
 const faunadb = require("faunadb")
 q = faunadb.query
-;(axios = require("axios")), require("dotenv").config()
+axios = require("axios"), 
+require("dotenv").config()
 
 const client = new faunadb.Client({
   secret: process.env.DB_SECRET,
