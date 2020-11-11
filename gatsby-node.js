aa882c7178c3a6ec4ff7e520334ac59402bb7d10
Muh-Hasan <muh.hasan.git@gmail.com>
2020-11-11 14:53:50 +0500

    ok

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,7 +10,7 @@ exports.createPages = async ({ actions, graphql }) => {
       }
     }
   `)
-
+console.log(data);
   data.Lollies.getAllLollies.forEach(({ lollyPath  }) => {
     actions.createPage({
       path: `lollies/${lollyPath}`,
