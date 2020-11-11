5d97d51ce8d792ba4f24fdc5127ca1d67b69828c
Muh-Hasan <muh.hasan.git@gmail.com>
2020-11-11 14:22:27 +0500

    error resolved

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   plugins: [
-    // {
-    //   resolve: "gatsby-source-graphql",
-    //   options: {
-    //     typeName: "Lolly",
-    //     fieldName: "Lollies",
-    //     url: "https://virtuallolly-serverless.netlify.app/.netlify/functions/newLolly",
-    //   },
-    // },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "Lolly",
+        fieldName: "Lollies",
+        url: "https://virtuallolly-serverless.netlify.app/.netlify/functions/newLolly",
+      },
+    },
   ],
 }
